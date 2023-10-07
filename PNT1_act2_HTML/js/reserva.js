7adbce3e8a4c5b4074a6bde98a6352353574380f
Jose Madrid <144069259+JD-Madrid@users.noreply.github.com>
2023-10-06 21:27:30 -0300

    Add files via upload
    Formulario de la pantalla de RESERVAS, (faltan hacer algunas validaciones)

--- PNT1_act2_HTML/js/reserva.js
+++ PNT1_act2_HTML/js/reserva.js
@@ -7,19 +7,21 @@ function hacerReserva() {
 function seleccionarSucursal() {
     let sectionSeleccionarSucursal = document.getElementById("seleccionar-sucursal")
     sectionSeleccionarSucursal.style.display = "none"
+    let hacerReservaSection = document.getElementById("hacer-reserva")
 
     let inputPalermo = document.getElementById("suc-palermo")
     let inputCaballito = document.getElementById("suc-caballito")
     let spanSucursalCliente = document.getElementById("reserva-cliente")
-    
+
     if (inputPalermo.checked) {
         spanSucursalCliente.innerHTML = "Sucursal Gurruchaga 1332 - Palermo"    
+        hacerReservaSection.style.display = "block"
     } else if (inputCaballito.checked) {
         spanSucursalCliente.innerHTML = "Sucursal Malvinas Argentinas 4040 - Caballito"
+        hacerReservaSection.style.display = "block"
     } else {
         alert("Selecciona una sucursal")
     }
 }
-
 
 window.addEventListener("load", hacerReserva)
